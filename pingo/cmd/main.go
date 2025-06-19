0af5bd1367939977d513f1b715103842bca565d4
Garage <bprosnitz+garage@gmail.com>
2025-06-19 09:52:50 -0700

    ok

--- pingo/cmd/main.go
+++ pingo/cmd/main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"os/signal"
 	"syscall"
@@ -15,7 +16,11 @@ import (
 
 func main() {
 	var timeout time.Duration
+	var sleep time.Duration
+	var n int
 	flag.DurationVar(&timeout, "timeout", 10*time.Second, "Amount of time to wait before cancelling the ping request")
+	flag.DurationVar(&sleep, "sleep", time.Second, "Amount of time to wait for the next ping")
+	flag.IntVar(&n, "n", math.MaxInt, "Number of pings to send")
 	flag.Parse()
 	if flag.NArg() != 1 {
 		log.Fatalf("expected target hostname arg")
@@ -24,9 +29,21 @@ func main() {
 
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer cancel()
-	ctx, cancel = context.WithTimeout(ctx, timeout)
-	defer cancel()
 
+	for i := 0; i < n; i++ {
+		select {
+		case <-ctx.Done():
+			return
+		case <-time.After(sleep):
+			break
+		}
+		sendPing(ctx, hostname, timeout)
+	}
+}
+
+func sendPing(ctx context.Context, hostname string, timeout time.Duration) {
+	ctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
 	elapsedTime, err := ping.Ping(ctx, hostname)
 	if err != nil {
 		log.Fatalf("error: %v", err)
